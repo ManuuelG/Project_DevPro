b3d3da4801ea25a17565b8b07e64466d0838c82c
ManuuelG <manuelguerreron93@gmail.com>
2023-12-21 16:00:08 +0100

    itnento deploy

--- client/src/Components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Navbar } from './Navbar'
-export { default as ProjectCard } from './ProjectCard'
-export { default as MultiSelect } from './MultiSelect'
-export { default as SearchBar } from './SearchBar'
-export { default as ProjectForm } from './ProjectForm'
